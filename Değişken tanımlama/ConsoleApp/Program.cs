4503bca7ffbda0d5ae562958ea670c4b19ed6d74
Burakbjkbt <usts3751@gmail.com>
2025-06-10 18:06:25 +0300

    Add files via upload

---
+++ Değişken tanımlama/ConsoleApp/Program.cs
@@ -0,0 +1,5 @@
+﻿var isim = "burak";
+Console.WriteLine(isim);
+/*1-Anahtar kelimemizdir*/
+/*2-Anahtar kelimeden sonra yazılıldığı kelime değişkenin ismi olur*/
+/* = den sonra bir veri tipi girilir*/
