9edd7ce4c4f7ffa4b07524a4d82a08b044c7ff04
Burakbjkbt <usts3751@gmail.com>
2025-06-12 15:48:31 +0300

    Add files via upload

---
+++ Veri Tipi Dönüşümü/ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("----------Metinsel Veri Tipi Dönüşümü Start----------");
+var metinsel_veri_tipinden_sayısal_veri_tipine_dönüşüm = Convert.ToInt32("10");
+Console.WriteLine(metinsel_veri_tipinden_sayısal_veri_tipine_dönüşüm);
+Console.WriteLine("----------Metinsel Veri Tipi Dönüşümü end----------");
+
+
+Console.WriteLine("----------Sayısal Veri Tipi Dönüşümü Start----------");
+var Ondalık_veri_tipinden_l_Tamsayı_veri_tipine_dönüşüm = 10.5;
+Ondalık_veri_tipinden_l_Tamsayı_veri_tipine_dönüşüm = Convert.ToInt32(Ondalık_veri_tipinden_l_Tamsayı_veri_tipine_dönüşüm);
+Console.WriteLine(Ondalık_veri_tipinden_l_Tamsayı_veri_tipine_dönüşüm);
+Console.WriteLine("----------Sayısal Veri Tipi Dönüşümü end----------");
+Console.WriteLine("----------Veri Tipi Dönüşümü end---------");
