a37013ccc765d8d6111ada6672e1d6fff851e801
Burakbjkbt <usts3751@gmail.com>
2025-06-12 15:27:48 +0300

    Delete Değişken tanımlama directory

--- Değişken tanımlama/ConsoleApp/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿var isim = "burak";
-Console.WriteLine(isim);
-/*1-Anahtar kelimemizdir*/
-/*2-Anahtar kelimeden sonra yazılıldığı kelime değişkenin ismi olur*/
-/* = den sonra bir veri tipi girilir*/
